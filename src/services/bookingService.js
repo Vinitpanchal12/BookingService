99a553747e1644b9986a3474d7ccad15830eb10c
Vinitjp123 <vinitjpanchal2002@gmail.com>
2023-09-22 16:51:44 +0530

    completed booking service

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -11,10 +11,25 @@ class BookingService{
     async createBooking(data){
         try {
             const flightId = data.flightId;
-            let getFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${data.flightId}`;
+            const getFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${data.flightId}`;
             const response = await axios.get(getFlightRequestUrl);
-            return response.data.data;
+            const flightData = response.data.data;
+            let priceOfTheFlight = flightData.price;
+            if(data.noOfSeats > flightData.totalSeats){
+                throw new ServiceError('Something went wrong in the booking process ', 'Insufficient seats in the flight');
+            }
+            const totalCost = priceOfTheFlight * data.noOfSeats;
+            const bookingPayload = {...data,totalCost};
+            const booking = await this.bookingRepository.create(bookingPayload); 
+            const updateFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
+            await axios.patch(updateFlightRequestUrl,{totalSeats:flightData.totalSeats -booking.noOfSeats});
+            const finalBooking = await this.bookingRepository.update(booking.id ,{status:'BOOKED'});
+            return finalBooking;
+
         } catch (error) {
+            if(error.name == 'ValidationError' || error.name =='RepositoryError'){   
+                throw error;
+            }
             throw new ServiceError(error);
         }
     }
